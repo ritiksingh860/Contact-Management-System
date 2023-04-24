ec8f167c4d8ef5d48ff88ab112e527cbdef57384
ritiksingh860 <94169027+ritiksingh860@users.noreply.github.com>
2023-04-24 15:13:50 +0530

    dse

--- src/main/java/com/springboot/cms/Application.java
+++ src/main/java/com/springboot/cms/Application.java
@@ -3,6 +3,7 @@ package com.springboot.cms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
