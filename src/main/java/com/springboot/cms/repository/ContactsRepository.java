2242877966d11ad9fe190dd9f51ef4bb0bffc243
ritiksingh860 <94169027+ritiksingh860@users.noreply.github.com>
2023-04-24 11:00:30 +0530

    add import

--- src/main/java/com/springboot/cms/repository/ContactsRepository.java
+++ src/main/java/com/springboot/cms/repository/ContactsRepository.java
@@ -4,6 +4,8 @@ import com.springboot.cms.model.Contacts;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ContactsRepository extends JpaRepository<Contacts, Long>{
   
