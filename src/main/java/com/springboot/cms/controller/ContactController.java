382b9ebfa467954f16bb71c4397f95bffe66022d
ritiksingh860 <94169027+ritiksingh860@users.noreply.github.com>
2023-04-24 10:59:29 +0530

    add import

--- src/main/java/com/springboot/cms/controller/ContactController.java
+++ src/main/java/com/springboot/cms/controller/ContactController.java
@@ -12,14 +12,7 @@ import com.springboot.cms.model.Contacts;
 import com.springboot.cms.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
