bff29a03479e94deb2d6396abb46f6f670536d39
ritiksingh860 <94169027+ritiksingh860@users.noreply.github.com>
2023-04-24 10:55:34 +0530

    asd

--- src/main/java/com/springboot/cms/controller/ContactController.java
+++ src/main/java/com/springboot/cms/controller/ContactController.java
@@ -20,6 +20,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 import com.springboot.cms.repository.ContactsRepository;
 
