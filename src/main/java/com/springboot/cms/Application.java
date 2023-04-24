e704decf466534bf4a18b9361b480b7630e822aa
ritiksingh860 <94169027+ritiksingh860@users.noreply.github.com>
2023-04-24 11:02:45 +0530

    add changes

--- src/main/java/com/springboot/cms/Application.java
+++ src/main/java/com/springboot/cms/Application.java
@@ -2,6 +2,7 @@ package com.springboot.cms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -10,11 +11,13 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableSwagger2
+@EnableAutoConfiguration
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
