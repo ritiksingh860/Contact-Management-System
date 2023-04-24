2280835c886819cf2758afbc8b3ecec1281619ca
ritiksingh860 <94169027+ritiksingh860@users.noreply.github.com>
2023-04-24 11:01:29 +0530

    add import

--- src/main/java/com/springboot/cms/service/impl/ContactServiceImpl.java
+++ src/main/java/com/springboot/cms/service/impl/ContactServiceImpl.java
@@ -8,6 +8,7 @@ import com.springboot.cms.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
